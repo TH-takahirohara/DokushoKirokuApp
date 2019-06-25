eea895d01a2c78ac5143538505a14277e8a35fb5
Takahiro Hara <takahiro.hb@gmail.com>
2019-06-25 19:46:40 +0900

    Initial Commit

---
+++ DokushoKirokuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DokushoKirokuApp
+//
+//  Created by 原昂大 on 2019/06/25.
+//  Copyright © 2019 takahiro.hara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DokushoKirokuAppTests/DokushoKirokuAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DokushoKirokuAppTests.swift
+//  DokushoKirokuAppTests
+//
+//  Created by 原昂大 on 2019/06/25.
+//  Copyright © 2019 takahiro.hara. All rights reserved.
+//
+
+import XCTest
+@testable import DokushoKirokuApp
+
+class DokushoKirokuAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
